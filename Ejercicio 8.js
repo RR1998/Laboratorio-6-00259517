1c5c69c96e3becd03343a5941c78167cd1bbf6db
Roberto Rubio <37192655+RR1998@users.noreply.github.com>
2018-09-28 08:58:15 -0600

    Add files via upload

---
+++ Ejercicio 8.js
@@ -0,0 +1,8 @@
+function matriz(matriz1, matriz2){
+    matriz3=[];
+    for (i=0; i<matriz1.length; i++){
+        for(j=0; j<matriz1[i].length; i++){
+            matriz3[i][j] = matriz1[i][j] + matriz2[i][j]; 
+        }
+    }
+}
